961573da9ceec433f1ee6546378a75ac1769d046
Offliner <42777325+Offliners@users.noreply.github.com>
2020-07-10 23:25:01 +0800

    Create ex2-17.c

---
+++ Chapter2/ex2-17.c
@@ -0,0 +1,28 @@
+include<stdio.h>                                                                                                                                                                                           
+
+double terminalV(double , double, double);
+double displacement(double, double, double);
+
+
+int main(void)
+{
+        double u, a, t;
+
+        printf("Please input initial velocity, acceleration and time (space-split) : ");
+        scanf("%lf %lf %lf", &u, &a, &t);
+
+        printf("Terminal Velocity = %lf\n", terminalV(u, a, t));
+        printf("Displacement = %lf\n", displacement(u, a, t));
+
+        return 0;
+}
+
+double terminalV(double u, double a, double t)
+{
+        return u + a*t;
+}
+
+double displacement(double u, double a, double t)
+{
+        return u*t + 0.5*a*t*t;
+}
