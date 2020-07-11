6d5d3f9acb4f508c0d3576af82ca5a3290fad7b9
Offliner <42777325+Offliners@users.noreply.github.com>
2020-07-11 12:41:36 +0800

    Create ex2-29.c

---
+++ Chapter2/ex2-29.c
@@ -0,0 +1,20 @@
+#include<stdio.h>                                                                                                                                                                                           
+
+int main(void)
+{
+        printf("%c : %d\n", 'A', 'A');
+        printf("%c : %d\n", 'B', 'B');
+        printf("%c : %d\n", 'C', 'C');
+        printf("%c : %d\n", 'a', 'a');
+        printf("%c : %d\n", 'b', 'b');
+        printf("%c : %d\n", 'c', 'c');
+        printf("%c : %d\n", '0', '0');
+        printf("%c : %d\n", '1', '1');
+        printf("%c : %d\n", '2', '2');
+        printf("%c : %d\n", '$', '$');
+        printf("%c : %d\n", '*', '*');
+        printf("%c : %d\n", '+', '+');
+        printf("%c : %d\n", '/', '/');
+
+        return 0;
+}
