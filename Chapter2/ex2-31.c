e874d05c559460d5b6e10157a027d6ba553d3e79
Offliner <42777325+Offliners@users.noreply.github.com>
2020-07-11 12:52:09 +0800

    Create ex2-31.c

---
+++ Chapter2/ex2-31.c
@@ -0,0 +1,11 @@
+#include<stdio.h>                                                                                                                                                                                           
+
+int main(void)
+{
+        printf("number\tsquare\tcube\n");
+
+        for(int i = 0; i < 11; i++)
+                printf("%d\t%d\t%d\n", i, i*i, i*i*i);
+
+        return 0;
+}
