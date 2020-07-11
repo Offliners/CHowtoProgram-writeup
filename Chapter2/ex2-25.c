6be28a0822fb5a0e0f97d72bc0854004e3eeeaac
Offliner <42777325+Offliners@users.noreply.github.com>
2020-07-11 12:22:28 +0800

    Create ex2-25.c

---
+++ Chapter2/ex2-25.c
@@ -0,0 +1,24 @@
+#include<stdio.h>                                                                                                                                                                                           
+
+int main(void)
+{
+        printf("PPPPPPPPP\n");
+        printf("    P   P\n");
+        printf("    P   P\n");
+        printf("    P   P\n");
+        printf("     PP\n");
+        printf("\n");
+        printf("  JJ\n");
+        printf(" J\n");
+        printf("J\n");
+        printf(" J\n");
+        printf("  JJJJJJJ\n");
+        printf("\n");
+        printf("DDDDDDDDD\n");
+        printf("D       D\n");
+        printf("D       D\n");
+        printf(" D     D\n");
+        printf("  DDDD\n");
+
+        return 0;
+}
