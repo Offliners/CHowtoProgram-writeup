6f5af34e1a067acb9e2cb89a177e15282f6bfe96
Offliner <42777325+Offliners@users.noreply.github.com>
2020-07-24 02:19:47 +0800

    Create ex4-38.c

---
+++ Chapter4/ex4-38.c
@@ -0,0 +1,158 @@
+#include<stdio.h>                                                                                                                                                                                           
+
+int main(void)
+{
+        printf("The Twelve Days of Christmas\n");
+        printf("========== Lyrics ==========\n");
+        printf("\n");
+
+        for(int i = 1; i <= 12; i++)
+        {
+                switch(i)
+                {
+                        case 1:
+                                printf("On the first day of Christmas\n");
+                                printf("My true love gave to me\n");
+                                printf("A partridge in a pear tree\n");
+                                printf("\n");
+                                break;
+                        case 2:
+                                printf("On the second day of Christmas\n");
+                                printf("My true love gave to me\n");
+                                printf("Two turtle doves\n");
+                                printf("And a partridge in a pear tree\n");
+                                printf("\n");
+                                break;
+                        case 3:
+                                printf("On the third day of Christmas\n");
+                                printf("My true love gave to me\n");
+                                printf("Three French hens\n");
+                                printf("Two turtle doves\n");
+                                printf("And a partridge in a pear tree\n");
+                                printf("\n");
+                                break;
+                        case 4:
+                                printf("On the fourth day of Christmas\n");
+                                printf("My true love gave to me\n");
+                                printf("Four calling birds\n");
+                                printf("Three French hens\n");
+                                printf("Two turtle doves\n");
+                                printf("And a partridge in a pear tree\n");
+                                printf("\n");
+                                break;
+                        case 5:
+                                printf("On the fifth day of Christmas\n");
+                                printf("My true love gave to me\n");
+                                printf("Five gold rings\n");
+                                printf("Four calling birds\n");
+                                printf("Three French hens\n");
+                                printf("Two turtle doves\n");
+                                printf("And a partridge in a pear tree\n");
+                                printf("\n");
+                                break;
+                        case 6:
+                                printf("On the sixth day of Christmas\n");
+                                printf("My true love gave to me\n");
+                                printf("Six geese a laying\n");
+                                printf("Five gold rings\n");
+                                printf("Four calling birds\n");
+                                printf("Three French hens\n");
+                                printf("Two turtle doves\n");
+                                printf("And a partridge in a pear tree\n");
+                                printf("\n");
+                                break;
+                        case 7:
+                                printf("On the seventh day of Christmas\n");
+                                printf("My true love gave to me\n");
+                                printf("Seven swans a swimming\n");
+                                printf("Six geese a laying\n");
+                                printf("Five gold rings\n");
+                                printf("Four calling birds\n");
+                                printf("Three French hens\n");
+                                printf("Two turtle doves\n");
+                                printf("And a partridge in a pear tree\n");
+                                printf("\n");
+                                break;
+                        case 8:
+                                printf("On the eighth day of Christmas\n");
+                                printf("My true love gave to me\n");
+                                printf("Eight maids a milking\n");
+                                printf("Seven swans a swimming\n");
+                                printf("Six geese a laying\n");
+                                printf("Five gold rings\n");
+                                printf("Four calling birds\n");
+                                printf("Three French hens\n");
+                                printf("Two turtle doves\n");
+                                printf("And a partridge in a pear tree\n");
+                                printf("\n");
+                                break;
+                            case 9:
+                                printf("On the ninth day of Christmas\n");
+                                printf("Me me me me me me\n");
+                                printf("Nine ladies dancing\n");
+                                printf("Eight maids a milking\n");
+                                printf("Seven swans a swimming\n");
+                                printf("Six geese a laying\n");
+                                printf("Five gold rings, badam-pam-pam\n");
+                                printf("Four calling birds\n");
+                                printf("Three French hens\n");
+                                printf("Two turtle doves\n");
+                                printf("And a partridge in a pear tree\n");
+                                printf("\n");
+                                break;
+                        case 10:
+                                printf("On the tenth day of Christmas\n");
+                                printf("My true love gave to me\n");
+                                printf("Ten lords a leaping\n");
+                                printf("Nine ladies dancing\n");
+                                printf("Eight maids a milking\n");
+                                printf("Seven swans a swimming\n");
+                                printf("Six geese a laying\n");
+                                printf("Five gold rings, badam-pam-pam\n");
+                                printf("Four calling birds\n");
+                                printf("Three French hens\n");
+                                printf("Two turtle doves\n");
+                                printf("And a partridge in a pear tree\n");
+                                printf("\n");
+                                break;
+                        case 11:
+                                printf("On the eleventh day of Christmas\n");
+                                printf("My true love gave to me\n");
+                                printf("Eleven pipers piping\n");
+                                printf("Ten lords a leaping\n");
+                                printf("Nine ladies dancing\n");
+                                printf("Eight maids a milking\n");
+                                printf("Seven swans a swimming\n");
+                                printf("Six geese a laying\n");
+                                printf("Five gold rings, badam-pam-pam\n");
+                                printf("Four calling birds\n");
+                                printf("Three French hens\n");
+                                printf("Two turtle doves\n");
+                                printf("And a partridge in a pear tree\n");
+                                printf("\n");
+                                break;
+                        case 12:
+                                printf("On the twelfth day of Christmas\n");
+                                printf("My true love gave to me\n");
+                                printf("12 drummers drumming\n");
+                                printf("Eleven pipers piping\n");
+                                printf("Ten lords a leaping\n");
+                                printf("Nine ladies dancing\n");
+                                printf("Eight maids a milking\n");
+                                printf("Seven swans a swimming\n");
+                                printf("Six geese a laying\n");
+                                printf("Five gold rings, badam-pam-pam\n");
+                                printf("Four calling birds\n");
+                                printf("Three French hens\n");
+                                printf("Two turtle doves\n");
+                                printf("And a partridge in a pear tree\n");
+                                printf("\n");
+                                break;
+                        default:
+                                printf("Error!\n");
+                                break;
+                }
+        }
+
+        return 0;
+}      
