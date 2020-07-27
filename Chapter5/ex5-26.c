8aaaebab4d4f4a30d3806d035fcf718d367026c1
Offliner <42777325+Offliners@users.noreply.github.com>
2020-07-28 01:38:04 +0800

    Create ex5-26.c

---
+++ Chapter5/ex5-26.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+#define MAXSIZE 10000
+
+int isPerfect(int);
+
+int main(void)
+{
+        printf("====== Perfect Number ======\n");
+        for(int i = 2; i <= MAXSIZE; i++)
+                if(isPerfect(i))
+                        printf(": %d\n", i);
+
+        return 0;
+}
+
+int isPerfect(int n)
+{
+        int divisor[MAXSIZE];
+        int count = 0;
+        int sum = 0;
+
+        for(int i = 1; i < n; i++)
+        {
+                if(n % i == 0)
+                {
+                        divisor[count] = i;
+                        count++;
+                }
+        }
+
+        for(int i = 0; i < count; i++)
+                sum += divisor[i];                                                                                                                                                                          
+
+        if(sum == n)
+        {
+                for(int i = 0; i < count; i++)
+                        printf("%d ", divisor[i]);
+
+                return 1;
+        }
+
+        else
+                return 0;
+}
